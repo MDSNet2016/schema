f3243250a275f0f59b9a50f77c63e458e8e0c6fd
dfr2 <rugg@musc.edu>
2013-06-05 14:26:48 -0400

    Changed schema into multi-level project

--- core/project.clj
+++ core/project.clj
@@ -1,5 +1,5 @@
-(defproject pliant.schema/parent "0.1.0-SNAPSHOT"
-  :description "Provides an extensible abstract schema and interface engine for data stores."
+(defproject pliant.schema/schema-core "0.1.0-SNAPSHOT"
+  :description "Provides an extensible abstract schema for data stores."
 
   :url "https://github.com/pliant/schema"
   :license {:name "Eclipse Public License"
@@ -12,10 +12,7 @@
   :jar-exclusions [#"^\." #"^*\/\." #"\.java$" #"project\.clj"]
   
   ;; To Run Marginalia us lein marg -d doc/api -m
-  :plugins [[lein-marginalia "0.7.1"]
-            [lein-sub "0.2.4"]]
-  
+  :plugins [[lein-marginalia "0.7.1"]]
   :aliases {"doc" ["marg" "-d" "doc/api" "-m"]}
-  
-  :sub ["core"])
 
+  :dependencies [[org.clojure/clojure "1.3.0"]])
